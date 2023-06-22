4cff1aa929e07579f13befc23b23c62ae8abd979
Salih Soner KARATAŞ <salihsonerkaratas@gmail.com>
2023-06-22 03:15:05 +0300

    Initial commit

---
+++ DataAccessLayer/Migrations/20230613154419_mig2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CaregoryID",
+                table: "Categories",
+                newName: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Categories",
+                newName: "CaregoryID");
+        }
+    }
+}
